8dcaa629eef8bb91cd42bb03209226dc79ec0c68
alyan <alyan@alyans-MacBook-Air.local>
2024-11-11 19:45:30 +0100

    final verison

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
     // vos configurations ici
-    DEFAULT_REMOTE_MODEL: "gpt-4o-2024-08-06"  // par exemple
+    DEFAULT_REMOTE_MODEL: "gpt-4o-mini-2024-07-18"  // par exemple
   };
